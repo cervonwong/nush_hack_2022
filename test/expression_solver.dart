41f1067f78cfad1dbda064118d25e4507eb126ff
Cervon Wong <tcrammy@gmail.com>
2022-11-11 15:53:03 +0800

    Remove default comments, make basic file structure

---
+++ test/expression_solver.dart
@@ -0,0 +1,15 @@
+int precedence(String c){
+  if(c == '+') {
+    return 1;
+  } else if (c == '*') {
+    return 2;
+  } else if (c == '!') {
+    return 3;
+  } else {
+    return -1;
+  }
+}
+
+void main(){
+  precen
+}
